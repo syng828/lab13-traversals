f5ffaa613ab5657d5ca1acac8d730cc5a956eafe
syng828 <syng828@gmail.com>
2023-04-28 11:31:04 -0700

    updated traversal code

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -140,7 +140,12 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	}
 	
 	public void inOrderRecurse(BSTNode<T> node) {
-		
+		if (node == null) { 
+			return;
+		}
+		inOrderRecurse(node.leftChild);
+		System.out.print(node.toString() + " "); 
+		inOrderRecurse(node.rightChild);
 	}
 	//Traverse the tree in an inorder fashion but using a stack
 	public void inOrderStack() {
